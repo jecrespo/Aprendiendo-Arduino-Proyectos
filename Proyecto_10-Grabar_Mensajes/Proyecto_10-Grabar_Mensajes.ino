d03b3b292329ea59bddf1b27588cda49c03215e8
Enrique Crespo <ecrespo@arsys.es>
2016-04-01 20:17:59 +0200

    Proyecto 10

--- Proyecto_10-Grabar_Mensajes/Proyecto_10-Grabar_Mensajes.ino
+++ Proyecto_10-Grabar_Mensajes/Proyecto_10-Grabar_Mensajes.ino
@@ -1,18 +1,26 @@
 #include <Ethernet.h>
 #include <SPI.h>
 
-byte mac[] = { 
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 
-  192, 168, 1, 179 };
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+byte ip[] = {
+  192, 168, 1, 179
+};
 byte DNS[] = {
-  8,8,8,8};
+  8, 8, 8, 8
+};
 byte gateway[] = {
-  192, 168, 1, 1};
+  192, 168, 1, 1
+};
 byte subnet[] = {
-  255,255,255,0};
+  255, 255, 255, 0
+};
 byte raspberry[] = {
-  192,168,1,169}; 
+  192, 168, 1, 22
+};
+char url[] = "www.aprendiendoarduino.com";
+byte aprendiendoarduino[] = { 217, 76, 132, 240};
 
 EthernetClient client;
 String webString = "";
@@ -26,58 +34,62 @@ void setup()
   delay(1000);
   Serial.println("Manda un mensaje al servidor.");
   Serial.println("Introduce Nombre:");
-  while (Serial.available() == 0){
+  while (Serial.available() == 0) {
   }
-  do{
+  do {
     char caracter_leido = Serial.read();
     nombre += caracter_leido;
     delay(5);
   }  while (Serial.available() > 0);
-  
+  Serial.println(nombre);
+
   Serial.println("Introduce Mensaje:");
-  while (Serial.available() == 0){
+  while (Serial.available() == 0) {
   }
-  do{
+  do {
     char caracter_leido = Serial.read();
     mensaje += caracter_leido;
     delay(5);
   }  while (Serial.available() > 0);
+  serial.println(mensaje);
+
   grabaDatos();
-  
-  while (client.available() == 0){
-  }
-  Serial.println("Respuesta del Servidor---->");
-  do{
-	char c = client.read();
-    webString += c;
-  }  while (client.available() > 0);
-  Serial.println(webString);
-  if (webString.endsWith("GRABADOS") == true) Serial.println("Datos guardados correctamente");
-  else Serial.println("Error al guardar los datos");
-   
-  client.stop();
+  delay(200);
 }
 
 void loop()
 {
 
+  if (client.available()) {
+    Serial.println("Respuesta del Servidor---->");
+    while (client.available()) {
+      char c = client.read();
+      webString += c
+    }
+    Serial.println(webString);
+    if (webString.endsWith("GRABADOS") == true) Serial.println("Datos guardados correctamente");
+    else Serial.println("Error al guardar los datos");
+
+    client.stop();
+
+  }
 }
 
-void grabaDatos(){
+void grabaDatos() {
   Serial.println("enviando mensaje... ");
-  
+
   Serial.println("connecting to server...");
   if (client.connect(raspberry, 80)) {
     Serial.println("connected");
-    client.print("GET /grabaMensajes.php?nombre=");
+    client.print("GET /servicio/grabaMensajes.php?nombre=");
     client.print(nombre);
-	client.print("&mensaje=");
-	client.print(mensaje);
+    client.print("&mensaje=");
+    client.print(mensaje);
     client.println(" HTTP/1.1");
-    client.println("Host: arduino");
+    client.println("Host: www.aprendiendoarduino.com");
     client.println("Connection: close");
     client.println();
-  } 
+  }
   else {
     Serial.println("connection failed");
   }
---
+++ Proyecto_10-Grabar_Raspberry/servidor_publico/servidor_publico.ino
@@ -0,0 +1,67 @@
+#include <Ethernet.h>
+#include <SPI.h>
+#include "Timer.h"
+
+
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 
+  192, 168, 1, 179 };
+byte DNS[] = {
+  8,8,8,8};
+byte gateway[] = {
+  192, 168, 1, 1};
+byte subnet[] = {
+  255,255,255,0};
+char url[] = "www.aprendiendoarduino.com";
+
+EthernetClient client;
+Timer t;
+String webString = "";
+
+void setup()
+{
+  Ethernet.begin(mac, ip, DNS, gateway, subnet);
+  Serial.begin(9600);
+  delay(1000);
+  t.every(5000,grabaDatos);
+}
+
+void loop()
+{
+  webString = "";
+  t.update();
+  if (client.available()) {
+    Serial.println("Respuesta del Servidor---->");
+    while (client.available()){
+      char c = client.read();
+      webString += c;
+    }
+    Serial.println(webString);
+    if (webString.indexOf("GRABADOS") > 0) Serial.println("Datos guardados correctamente");
+    else Serial.println("Error al guardar los datos");
+    
+   client.stop();
+  }
+}
+
+void grabaDatos(){
+  Serial.println("leyendo temperatura... ");
+  int sensorVal = analogRead(A0);
+  Serial.print("Leido: ");
+  Serial.println(sensorVal);
+  
+  Serial.println("connecting to server...");
+  if (client.connect(url, 80)) {
+    Serial.println("connected");
+    client.print("GET /servicios/grabaDatos.php?arduino=99&dato=");
+    client.print(sensorVal);
+    client.println(" HTTP/1.1");
+    client.println("Host: www.aprendiendoarduino.com");
+    client.println("Connection: close");
+    client.println();
+  } 
+  else {
+    Serial.println("connection failed");
+  }
+}
